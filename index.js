f7c092364e2412d09b2cce8a7dbdc27ad99f9d93
Rahul <rahul.wa47@gmail.com>
2025-07-09 17:38:30 +0530

    created a gameboard module to test in console

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hello world')
+
+
+const gameBoard = (function () {
+  const cells = ["", "", "", "", "", "", "", "", ""];
+  
+  const updateCells = (symbol, position) => {
+    if(cells[position].length){
+      return
+    }else {
+      return cells[position] = symbol
+    }
+    
+  }
+  return {cells, updateCells}
+
+})() 
+
+gameBoard.updateCells("x", 2);
+
+console.log(gameBoard.cells);
+
+gameBoard.updateCells("o", 2);
+
+console.log(gameBoard.cells);
