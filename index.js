9c952d6cf1c13feb3e8df85218bdc690f47fe87c
Rahul <rahul.wa47@gmail.com>
2025-07-09 22:03:26 +0530

    game controller now check if the game board contains a winning combo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 const gameBoard = (function () {
-  const cells = ["", "", "", "", "", "", "", "", ""];
+  const cells = ["x", "", "", "x", "", "", "x", "", ""];
   
   const updateCells = (symbol, position) => {
     if(cells[position].length){
@@ -15,10 +15,52 @@ const gameBoard = (function () {
 
 })() 
 
-gameBoard.updateCells("x", 2);
 
-console.log(gameBoard.cells);
 
-gameBoard.updateCells("o", 2);
+// 
 
-console.log(gameBoard.cells);
+// gameBoard.updateCells("o", 2);
+
+// console.log(gameBoard.cells);
+
+const player = function (symbol) {
+  return {symbol}
+}
+
+const gameController = (function () {
+
+  const winningCombination = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+  const currentCells = gameBoard.cells
+
+  const checkWinner = (currSymbol) => {
+    let winner = "none";
+    winningCombination.map((combo) => {
+      const checkCombo = combo.every((cell) => currentCells[cell] === currSymbol)
+      if(checkCombo) {
+        winner = currSymbol;
+      }
+    }
+    )
+    return winner;
+  }
+
+  return {checkWinner}
+})()
+
+console.log(gameController.checkWinner('x'));
+
+
+
+// const player1 = player("x")
+// const player2 = player('o')
+
+// console.log(player1.symbol)
+// console.log(player2.symbol);
+
+// gameBoard.updateCells(player1.symbol, 2);
+
+// console.log(gameController.checkWinner());
+
+// gameBoard.updateCells(player2.symbol, 3);
+
+// console.log(gameBoard.cells);
